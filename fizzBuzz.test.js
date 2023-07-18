dee544ca682f80d3577dc04218d59beea16e6ed4
dava <davaasuren.dorjdagva@and.digital>
2023-07-18 11:11:28 +0100

    added divisible by 3

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,11 +1,16 @@
 const prod_code = (number) => {
-
+if(number % 3 == 0){
+    return true;
+}
+return false;
 };
 
 describe('Fizz Buzz', () => {
-    it('', () => {
+    it('Returns Fizz if the number is divisible by 3', () => {
         // Arrange
         // Act
         // Assert
+        expect(prod_code(3)).toBe('thing');
     });
+
 });
