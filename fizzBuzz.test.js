61a94c83d19cd8bc5968f294c3a719cb9bd4794d
Maeve Buatsi <maeve.b@yahoo.com>
2023-07-18 11:42:03 +0100

    added Buzz if divisible by five

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,16 +1,28 @@
 const prod_code = (number) => {
 if(number % 3 == 0){
-    return true;
+    return "Fizz";
 }
+if(number % 5 == 0){
+    return "Buzz";
+}
+
 return false;
 };
 
 describe('Fizz Buzz', () => {
+
     it('Returns Fizz if the number is divisible by 3', () => {
+        expect(prod_code(3)).toBe("Fizz");
+    });
+
+    it('Returns Buzz if the number is divisible by 5', () => {
+        expect(prod_code(5)).toBe("Buzz");
+    });
+
+
         // Arrange
         // Act
         // Assert
-        expect(prod_code(3)).toBe('thing');
-    });
+
 
 });
